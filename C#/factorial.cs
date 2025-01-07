dbf62f7d395889b51cabdf80db4ac76c122a485f
Saebtimar77 <saebtimar77@gmail.com>
2025-01-07 12:46:16 +0330

    Add files via upload

---
+++ C#/factorial.cs
@@ -0,0 +1,14 @@
+﻿// Factorial
+
+Console.Write("Enter your number :");
+
+int number =int.Parse(Console.ReadLine());
+
+int result = number;
+Console.WriteLine(result);
+for (int counter = number-1; counter > 0 ; counter--)
+{
+    Console.Write($"*{counter}");
+    result = result * counter;
+}
+Console.Write($" =  {result}");
